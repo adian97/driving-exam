22208537629daacebe6bda8b2fd8a23e10020715
adian97 <143320371+adian97@users.noreply.github.com>
2026-06-24 21:16:49 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,2 +1,30 @@
+async function login() {
 
-// login javascript
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    const response =
+        await fetch("data/login.json");
+
+    const data =
+        await response.json();
+
+    if (
+        username === data.username &&
+        password === data.password
+    ) {
+        sessionStorage.setItem(
+            "loggedIn",
+            "true"
+        );
+
+        window.location.href =
+            "main.html";
+    }
+    else {
+        alert("Wrong username or password.");
+    }
+}
