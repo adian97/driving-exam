b24f41c0826f389320a2e4788e3be3346f196dea
adian97 <143320371+adian97@users.noreply.github.com>
2026-06-24 21:28:03 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+if (sessionStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "index.html";
+}
 
+function goExam() {
+    sessionStorage.setItem("mode", "exam");
+    window.location.href = "category.html";
+}
+
+function goPractice() {
+    sessionStorage.setItem("mode", "practice");
+    window.location.href = "category.html";
+}
+
+function logout() {
+    sessionStorage.clear();
+    window.location.href = "index.html";
+}
