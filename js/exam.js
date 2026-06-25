2197a161c9653ffd72d35dea4ca1da24c05e74a8
adian97 <143320371+adian97@users.noreply.github.com>
2026-06-25 22:08:48 +0600

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -1,4 +1,10 @@
-for(let i=1;i<=30;i++){
+let currentQuestion = 0;
+let totalQuestions = 30;
+
+const questionNumbers =
+    document.getElementById("questionNumbers");
+
+for(let i=0;i<totalQuestions;i++){
 
     const div =
         document.createElement("div");
@@ -6,12 +12,36 @@ for(let i=1;i<=30;i++){
     div.className =
         "question-box-item";
 
-    if(i===1)
+    if(i===0){
         div.classList.add("active");
+    }
 
-    div.innerText = i;
+    div.innerText = i + 1;
+
+    div.onclick = function(){
+        showQuestion(i);
+    };
+
+    questionNumbers.appendChild(div);
+}
+
+function showQuestion(index){
+
+    currentQuestion = index;
 
     document
-        .getElementById("questionNumbers")
-        .appendChild(div);
+        .querySelectorAll(".question-box-item")
+        .forEach(box=>{
+            box.classList.remove("active");
+        });
+
+    document
+        .querySelectorAll(".question-box-item")
+        [index]
+        .classList
+        .add("active");
+
+    document
+        .getElementById("questionNo")
+        .innerText = index + 1;
 }
