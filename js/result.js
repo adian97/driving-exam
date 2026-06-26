94246f5b952f612bf6de451e5750bc31ef85222e
Faruque Ahmed <143320371+adian97@users.noreply.github.com>
2026-06-26 15:20:20 +0600

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,2 +1,63 @@
+const correct =
+    Number(
+        sessionStorage.getItem(
+            "correctAnswers"
+        )
+    );
 
-// exam javascript// exam javascript
+const wrong =
+    Number(
+        sessionStorage.getItem(
+            "wrongAnswers"
+        )
+    );
+
+const total =
+    correct + wrong;
+
+const score =
+    Math.round(
+        (correct / total) * 100
+    );
+
+document.getElementById(
+    "correctText"
+).innerText = correct;
+
+document.getElementById(
+    "wrongText"
+).innerText = wrong;
+
+document.getElementById(
+    "scoreText"
+).innerText =
+    score + "%";
+
+if (score >= 74) {
+    document.getElementById(
+        "resultStatus"
+    ).innerText = "PASSED";
+}
+else {
+    document.getElementById(
+        "resultStatus"
+    ).innerText = "FAILED";
+    document.getElementById(
+        "resultStatus"
+    ).style.color = "red";
+}
+
+function reviewAnswers() {
+    window.location.href =
+        "review.html";
+}
+
+function tryAgain() {
+    window.location.href =
+        "exam.html";
+}
+
+function goHome() {
+    window.location.href =
+        "main.html";
+}
