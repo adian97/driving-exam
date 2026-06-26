ff4a18a04bd9dc518d5f2b97234f0d69411a5ca2
Faruque Ahmed <143320371+adian97@users.noreply.github.com>
2026-06-26 18:39:58 +0600

    Update review.js

--- js/review.js
+++ js/review.js
@@ -13,3 +13,44 @@ let userAnswers =
     ) || [];
 
 let currentQuestion = 0;
+function createReviewNumbers() {
+
+    const container =
+        document.getElementById(
+            "reviewNumbers"
+        );
+
+    container.innerHTML = "";
+
+    questions.forEach((question, index) => {
+
+        const div =
+            document.createElement("div");
+
+        div.className =
+            "review-number";
+
+        div.innerText =
+            index + 1;
+
+        if (
+            userAnswers[index] ===
+            question.correctAnswer
+        ) {
+            div.classList.add(
+                "correct"
+            );
+        }
+        else {
+            div.classList.add(
+                "wrong"
+            );
+        }
+
+        div.onclick = function () {
+            showReviewQuestion(index);
+        };
+
+        container.appendChild(div);
+    });
+}
