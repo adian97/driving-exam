d777d3785fd21de44e6af38c197e2cdcf853a842
Faruque Ahmed <143320371+adian97@users.noreply.github.com>
2026-06-27 15:22:10 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,3 +28,48 @@ function logout() {
     sessionStorage.clear();
     window.location.href = "index.html";
 }
+/* LOAD STATISTICS */
+
+const examsTaken =
+    Number(
+        localStorage.getItem(
+            "examsTaken"
+        )
+    ) || 0;
+
+const totalScore =
+    Number(
+        localStorage.getItem(
+            "totalScore"
+        )
+    ) || 0;
+
+const bestScore =
+    Number(
+        localStorage.getItem(
+            "bestScore"
+        )
+    ) || 0;
+
+const averageScore =
+    examsTaken > 0
+        ? Math.round(
+            totalScore /
+            examsTaken
+        )
+        : 0;
+
+document.getElementById(
+    "examsTaken"
+).innerText =
+    examsTaken;
+
+document.getElementById(
+    "averageScore"
+).innerText =
+    averageScore + "%";
+
+document.getElementById(
+    "bestScore"
+).innerText =
+    bestScore;
